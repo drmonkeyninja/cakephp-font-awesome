2006a17563676d6d035d5391b9a917fb1fc96fb4
Andy Carter <andy@evoluted.net>
2017-02-17 10:36:14 +0000

    Updated tests

--- tests/TestCase/View/Helper/FaHelperTest.php
+++ tests/TestCase/View/Helper/FaHelperTest.php
@@ -20,20 +20,20 @@ class FaHelperTest extends TestCase
 
     public function testLink()
     {
-        $result = $this->Fa->link('test', 'Proceed', array('controller' => 'foo', 'action' => 'bar'));
+        $result = $this->Fa->link('test', 'Proceed', '/foo/bar');
         $expected = '<a href="/foo/bar">Proceed <i class="fa fa-test"></i></a>';
         $this->assertEquals($expected, $result);
 
         // Test link text gets escaped correctly.
-        $result = $this->Fa->link('test', '>', array('controller' => 'foo', 'action' => 'bar'));
+        $result = $this->Fa->link('test', '>', '/foo/bar');
         $expected = '<a href="/foo/bar">&gt; <i class="fa fa-test"></i></a>';
         $this->assertEquals($expected, $result);
 
-        $result = $this->Fa->link('test', 'Proceed', array('controller' => 'foo', 'action' => 'bar'), array('before' => true));
+        $result = $this->Fa->link('test', 'Proceed', '/foo/bar', ['before' => true]);
         $expected = '<a href="/foo/bar"><i class="fa fa-test"></i> Proceed</a>';
         $this->assertEquals($expected, $result);
 
-        $result = $this->Fa->link('test', null, array('controller' => 'foo', 'action' => 'bar'), array('before' => true));
+        $result = $this->Fa->link('test', null, '/foo/bar', ['before' => true]);
         $expected = '<a href="/foo/bar"><i class="fa fa-test"></i></a>';
         $this->assertEquals($expected, $result);
     }
